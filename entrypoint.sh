404ea1b0d08e88ed68d266d27f07ef6ed87c4a9c
Hoang Yen Nhi <yennhi03013009@gmail.com>
2026-05-26 19:46:32 +0700

    Edit entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,14 @@ SPARK_WORKLOAD=$1
 echo "SPARK_WORKLOAD: $SPARK_WORKLOAD"
 
 if [ "$SPARK_WORKLOAD" == "master" ]; then
-  echo "Starting Spark Master..."
-  start-master.sh -p 7077
+  echo "Starting Spark Master in FOREGROUND..."
+  exec spark-class org.apache.spark.deploy.master.Master --host spark-master --port 7077 --webui-port 8080
 elif [[ "$SPARK_WORKLOAD" == worker* ]]; then
-  # Start worker with the appropriate worker name (worker-1, worker-2, worker-3)
-  echo "Starting Spark Worker: $SPARK_WORKLOAD"
-  start-worker.sh spark://spark-master:7077
+  echo "Starting Spark Worker in FOREGROUND..."
+  exec spark-class org.apache.spark.deploy.worker.Worker spark://spark-master:7077
 elif [ "$SPARK_WORKLOAD" == "history" ]; then
   echo "Starting Spark History Server..."
-  start-history-server.sh
+  exec spark-class org.apache.spark.deploy.history.HistoryServer
 else
   echo "Unknown workload: $SPARK_WORKLOAD"
   exit 1
